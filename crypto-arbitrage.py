031248ce48df3a96cc4a1bbe8d92387961907c03
Elton Tundu <mcwilton85@gmail.com>
2023-08-14 17:57:44 -0700

    Update crypto-arbitrage.py
    Latest update

--- crypto-arbitrage.py
+++ crypto-arbitrage.py
@@ -1,99 +1,54 @@
 import ccxt
 from twilio.rest import Client
 import math
+import ccxt  # Cryptocurrency Exchange Library
+import time
 
-#kraken api - add your api keys below
-kraken = ccxt.kraken({
-    'apiKey': '*******',
-    'secret': '*******',
-})
+# Replace with your actual API keys
+api_key_exchange1 = 'YOUR_API_KEY1'
+api_secret_exchange1 = 'YOUR_API_SECRET1'
+api_key_exchange2 = 'YOUR_API_KEY2'
+api_secret_exchange2 = 'YOUR_API_SECRET2'
 
-#bitrex api - add your api keys below
-bitrex = ccxt.bitrex({
-    'apiKey': '*******',
-    'secret': '*******',
-})
+exchange1 = ccxt.binance({'apiKey': api_key_exchange1, 'secret': api_secret_exchange1})
+exchange2 = ccxt.kraken({'apiKey': api_key_exchange2, 'secret': api_secret_exchange2})
 
-#Twilio sms api. Add your own api here
-account_sid = '******'
-auth_token = '******'
-client = Client(account_sid, auth_token)
+def get_balances(exchange):
+    balances = exchange.fetch_balance()
+    return balances['total']
 
+def get_prices(exchange1, exchange2, symbol):
+    ticker1 = exchange1.fetch_ticker(symbol)
+    ticker2 = exchange2.fetch_ticker(symbol)
+    return ticker1['bid'], ticker2['ask']
 
-def printsep():
-    print("====================================================")
+def execute_arbitrage(symbol):
+    while True:
+        try:
+            balance_exchange1 = get_balances(exchange1)
+            balance_exchange2 = get_balances(exchange2)
 
+            bid_price, ask_price = get_prices(exchange1, exchange2, symbol)
 
-bittrex = ccxt.bittrex()
-kraken = ccxt.kraken()
+            if bid_price > ask_price:
+                amount_to_buy = min(balance_exchange2[symbol.split("/")[0]], balance_exchange1[symbol.split("/")[1]])
+                if amount_to_buy > 0:
+                    # Execute buy on exchange2 and sell on exchange1
+                    exchange2.create_market_buy_order(symbol, amount_to_buy)
+                    exchange1.create_market_sell_order(symbol, amount_to_buy)
 
-bittrex_markets = bittrex.load_markets()
-kraken_markets = kraken.load_markets()
+                    print(f"Arbitrage opportunity found!")
+                    print(f"Buy on Exchange2 at {ask_price} and sell on Exchange1 at {bid_price}")
+                else:
+                    print("Insufficient balance for arbitrage.")
+            else:
+                print("No arbitrage opportunity at the moment.")
 
-#find last price
-kraken_price = kraken.fetch_ticker('BTC/USD')['close']
+            time.sleep(60)  # Wait for a minute before checking again
 
-bitrex_price = bittrex.fetch_ticker('BTC/USD')['close']
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-#check on fees charged per transaction
-kraken_fees = kraken.markets['BTC/USD']['maker']
-
-bitrex_fees = bittrex.markets['BTC/USD']['maker']
-
-#check for arbitraging
-if kraken_price < bitrex_price:
-	cross_exchange_arbitrage = (bitrex_price - kraken_price)/100
-	buy = ("Buy on Kraken and sell on Bitrex. Your fees will be: " + str(kraken_fees))
-	gain = bitrex_price - kraken_price
-#	bittrex.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
-
-#send an sms signal
-	message = client.messages \
-	.create(
-                body="Buy Bitcoin on Kraken @ " + str(kraken_price) + " and sell on Bitrex @ " + str(bitrex_price) + " Arbitrage: " +  "{:.2f}".format(cross_exchange_arbitrage) + "%",
-                from_= '*******',   #add your twilio number e.g +27 00 000 0000
-                to= '*******',    #add your cell number to receive sms e.g +27 00 000 0000
-            )
-
-else:
-	cross_exchange_arbitrage = (kraken_price - bitrex_price)/100
-	buy = ("Buy on Bittrex and sell on Kraken. Your fees will be: " + str(bitrex_fees))
-	gain = kraken_price - bitrex_price
-# 	kraken.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
-		
-	message = client.messages \
-            .create(
-            	body="Buy Bitcoin  on Bittrex @ " + str(kraken_price) + " and sell on Kraken @ " + str(bitrex_price) + " Arbitrage: " +  "{:.2f}".format(cross_exchange_arbitrage) + "%",
-            	from_= '*******',   #add your twilio number  e.g +27 00 000 0000
-                to= '*******',    #add your cell number to receive sms  e.g +27 00 000 0000
-            )
-
-print("Cross Exchange Opportunity: ",  buy, "your gain is: $", math.floor(gain))
-print("Bittrex price is: ", kraken_price,"---", "Kraken price is:", bitrex_price)
-print("You arbitrage: ", "{:.2f}".format(cross_exchange_arbitrage), "%")
-printsep()  
-
-# Here we start the code for buying and selling on a single exchange. Thresholds have to be set first as targets for trades.
-sell_bitcoin_threshold = 19300 # change to the one you want
-buy_bitcoin_threshold = 19045  #change to the one you want
-
-if kraken_price <= buy_bitcoin_threshold:
-	print("opportunity to buy Bitcoin on Kraken")
-	#run a buy order on signal
-#	kraken.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'}) 
-
-elif kraken_price >= sell_bitcoin_threshold:
-	print("Opportunity to sell Bitcoin on Bittrex")
-#	kraken.create_market_sell_order('BTC/USD', 1, {'trading_agreement': 'agree'})
-
-elif bitrex_price <= buy_bitcoin_threshold:
-	print("Opportunity to buy Bitcoin on Kraken")
-#	bitrex.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
-	
-elif bitrex_price >= sell_bitcoin_threshold:
-	print("Opportunity to sell Bitcoin on Bittrex")
-#	kraken.create_market_sell_order('BTC/USD', 1, {'trading_agreement': 'agree'})
-	
-else:
-	print("No opportunities in single exchange is presented as yet")
-
+if __name__ == "__main__":
+    symbol = "BTC/USDT"  # Replace with the trading pair you're interested in
+    execute_arbitrage(symbol)
